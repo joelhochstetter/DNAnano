2b884cefe7f2d0978e163e430dd6c4d67b1ccc97
Joel Hochstetter <joel.hochstetter97@gmail.com>
2020-06-03 13:49:03 +1000

    Update files

--- oxDNA/MyScripts/rowSqsSameNt.py
+++ oxDNA/MyScripts/rowSqsSameNt.py
@@ -1,39 +1,26 @@
 '''
-This script takes a json file of arbitrary tether length and output files of specified tether length.
-The tether must already be connected out of the pore for this to work. This does not change cinches or handles. I plan to write additional code to do this. 
+This takes an oxDNA sequence file and sets row to all same nucleotide
 
 Inputs:
-    jsonfile - the json file for the cadnano design
-    n        - The tether 'half length' as number of nucleotides 
-    t5p_p    - vstrand (row) index for coming out of pore in 3' to 5' direction. This will be connected to the square in the pore (on an even strand if connected to left of pore). This should be an even numbered strand.
-    t5p_b    - vstrand (row) index for coming out of pore in 3' to 5' direction. This is connected to  't5p_p' and it's end nucleotide is fixed. This should be an odd numbered strand.     
-    t3p_p    - vstrand (row) index for coming out of pore in 5' to 3' direction. This will be connected to the arrow in the pore (on an even strand if connected to left of pore). This should be an odd numbered strand.
-    t3p_b    - vstrand (row) index for coming out of pore in 5' to 3' direction. This is connected to  't3p_p' and it's end nucleotide is fixed. This should be an even numbered strand.     
-
-
- 
-Usage: python changeTether.py jsonfile n t3p_p t3p_b t5p_p t5p_b 
-
-For the default pore: 
-- Pore helices 0-5
-- Tether helices 6-9
-usage is: python changeTether.py jsonfile n 6 7 9 8
+    seqFile: 'casa.sqs' the sequence file to use in oxDNA
+    base: Either A, G, C, T, or R (random)
+    row
 
 Last edited: 7/2/20
 '''
 
 
-def changeTether():
-    import sys
-    import json
-    import math
-    print(sys.argv)
-    jsonfile  = sys.argv[1]
-    teth_len  = int(sys.argv[2])
-    t5p_p     = int(sys.argv[3])
-    t5p_b     = int(sys.argv[4])
-    t3p_p     = int(sys.argv[5])
-    t3p_b     = int(sys.argv[6])
+import sys
+
+seqFile  = sys.argv[1]
+base  = int(sys.argv[2])
+t5p_p     = int(sys.argv[3])
+t5p_b     = int(sys.argv[4])
+t3p_p     = int(sys.argv[5])
+t3p_b     = int(sys.argv[6])
+
+def rowSameNT():
+
      
     #partition tether length into halves (1) and (2)
     #lenT_p is length of strand which connects to pore
